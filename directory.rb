3700a771dc0effef5215825a08f837533276b20c
Dillon Barker <barker.dill98@gmail.com>
2020-06-29 10:10:53 +0100

    adding comments and a total number of students to cleaned up directory.rb

--- directory.rb
+++ directory.rb
@@ -1,3 +1,4 @@
+student_count = 11
 # Printing the list of students
 puts "The students of Villains Academy"
 puts "-------------"
@@ -12,7 +13,5 @@ puts "Freddy Krueger"
 puts "The Joker"
 puts "Joffrey Baratheon"
 puts "Norman Bates"
-# Printing the total number of students all on one line!
-print "Overall, we have "
-print 11
-puts " great students"
+# finally, we print the total number of students
+puts "Overall, we have #{student_count} great students"
